00c3db6096cb040ed6bde6be1bbbfe098bb12742
cobe44 <cobe44@gmail.com>
2018-02-25 17:07:52 -0500

    Added date integer conversion function

--- curve_fit.R
+++ curve_fit.R
@@ -6,7 +6,7 @@ library('tidyr')
 library('ggplot2')
 
 #load argument parser
-parser <- ArgumentParser(description="TESTING")
+parser <- ArgumentParser(description="Create a set of polynomial features out of numerical data in tabular format (csv please). Specify the degree of the polynomial you want (-m), optionally the columns you want (-c).")
 
 #Specify options
 parser$add_argument("-i", "--infile", type="character", nargs=1,
@@ -98,7 +98,8 @@ show_hist <- function(column, stepsize){
 }
 
 ID_filter <- function(dataframe){
-  #Keep only rows with IDs that occur number of times specified by tolerance flag (default 2; can't fit a curve to a point)
+  #Keep only rows with IDs that occur number of times specified by degree of desired polynomial fit
+  #Ex.: If you have two points there's no reason to fit a parabola to them. Same with three points and a cubic.
   new_md <- dataframe[dataframe$ID %in% names(table(dataframe$ID))[table(dataframe$ID) >= tolerance],]
 }
 
@@ -143,9 +144,24 @@ if(disp_numeric || binary || nonbinary){stop()}
 
 if(columns[[1]] == "ALL"){
   print("No columns specified. Proceeding with analysis on all continuous numerical columns.")
+
+  #Store dates separately
+  ID_and_dates <- metadata[c("ID", "Visit_DT")]
+  print(nrow(ID_and_dates))
+
+
   #Select columns from metadata which contain numeric variables and no NAs
-  reduced_meta_unfiltered <- metadata %>% select_if(function(col) all(is.numeric(col)))
+  numeric_without_dates <- metadata %>% select_if(function(col) all(is.numeric(col)))
+  #Stick the dates column between the ID column and everything else
+  reduced_meta_unfiltered <- cbind(ID_and_dates, subset(numeric_without_dates, select=-ID))
+  #Filter out rows with inadequate n_samples
   reduced_meta <- ID_filter(reduced_meta_unfiltered)
+
+  #Remove redundant information; no need for visit IDs
+  #What to do about this in the future?
+  reduced_meta$Visit <- NULL
+  reduced_meta$ID_visit <- NULL
+
   if(boxplot){
     show_boxplot(reduced_meta)
   }
@@ -158,10 +174,10 @@ if(columns[[1]] == "ALL"){
 
 } else{
   #Select only specified columns and ID column
-  reduced_meta_wnans_unfiltered <- metadata[c("ID",args$columns)]
-  reduced_meta_wnans <- ID_filter(reduced_meta_wnans_unfiltered)
-  #Remove rows with one column containing NA
-  reduced_meta <- reduced_meta_wnans %>% drop_na()
+  reduced_meta_wnans_unfiltered <- metadata[c("ID", "Visit_DT", args$columns)]
+  #Remove rows corresponding to IDs with insufficient n_samples
+  reduced_meta <- ID_filter(reduced_meta_wnans_unfiltered)
+
   if(boxplot){
     show_boxplot(reduced_meta)
   }
@@ -180,19 +196,31 @@ if(columns[[1]] == "ALL"){
 #List of relevant IDs
 ID_list <- unique(reduced_meta$ID)
 
+#Create date lookup table
+date.lookup <- format(seq(as.Date("1980-01-01"), as.Date("2025-01-01"), by="1 day"))
+
 ############################################
 #               function zoo               #
 ############################################
 
-curve_fit <- function(df_column){
+date_range_constructor <- function(date){
+  #Convert date to Date class
+  form_date <- as.Date(date, format="%m/%d/%y")
+  #Return integer value from lookup table; must convert date class to character prior to lookup
+  return(match(as.character(form_date), date.lookup))
+}
+
+curve_fit <- function(df_column, dates){
   if(mode == "linear"){
     if(any(is.na(df_column))){
       #Don't do any analysis if at least one row of column contains an NA field
       return(list(NA, NA))
     }
-    range <- seq(1, length(df_column), by=1)
+
     #Perform linear least-squares regression
-    curve <- lm(df_column ~ range)
+    curve <- lm(df_column ~ dates)
+    print(curve)
+    stop()
     #Make list of coefficients (stored as list of lists)
     comp_coeff <- list(curve$coefficients[[1]], curve$coefficients[[2]])
     #Undo list comprehension
@@ -208,7 +236,6 @@ ID_processor <- function(ID, ID_DF, header){
   #Operates sequentially on IDs; sends one set of data belonging to a particular ID to the curve_fit function
 
   #Only operate on one ID per pass
-
   ID_red_DF <- ID_DF[ID_DF$ID == ID,]
 
   #Create reduced dataframe consisting of only ID column; add columns progressively with fit parameters
@@ -222,7 +249,7 @@ ID_processor <- function(ID, ID_DF, header){
   colnames(new_row) <- header
 
   #Generate row with curve fit parameter data
-  ID_row_badheader <- lapply(sans_ID, FUN=curve_fit)
+  ID_row_badheader <- lapply(sans_ID, FUN=curve_fit, dates=ID_red_DF$Visit_DT)
   new_row$ID <- ID
   new_row[1,2:length(new_row)] <- unlist(ID_row_badheader)
 }
@@ -258,11 +285,19 @@ param_df_constructor <- function(reduced_meta, ID_list){
   header <- c(ID,header)
   header <- unlist(header)
 
+  #Convert dates to integers from lookup table for later conversion back to dates
+  reduced_meta$Visit_DT <- unlist(lapply(reduced_meta$Visit_DT, FUN=date_range_constructor))
+
   #Pass data and ID_list to downstream functions to create curve-fit dataframe
   #NOT READY YET
   df_without_header <- lapply(ID_list, FUN=ID_processor, ID_DF=reduced_meta, header=header)
+  #Turn df_without_header from list into data.frame
   df_without_header <- do.call("rbind", df_without_header)
-  param_df <- cbind(ID_list, df_without_header)
+  #Attach list of unique IDs to df_without_header
+  param_df <- data.frame(cbind(ID_list, df_without_header))
+  print(names(param_df))
+  stop()
+  #Add column names (generated by header_name_generator)
   colnames(param_df) <- header
   return(param_df)
 }
@@ -271,6 +306,7 @@ spaghetti <- function(param_df){
   #Need date/time. Try making a scatterplot first, then worry about the lines.
   return
 }
+
 
 #Create DF with rows corresponding to IDs and columns containing curve parameters
 param_df <- param_df_constructor(reduced_meta, ID_list)
